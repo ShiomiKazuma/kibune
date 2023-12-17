225a68e006d008d0a1a974385fc2c5f49194af2f
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2023-12-17 20:46:45 +0900

    [add] オブジェクトプールを活用した敵スポナーの実装中

---
+++ Assets/RoofESpawner.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoofESpawner : MonoBehaviour
+{
+    [SerializeField] uint _maxAmount;
+    [SerializeField] GameObject _enemyObject;
+    Stack<GameObject> _stackedObj = new Stack<GameObject>();
+
+
+    void SetUpPooling()
+    {
+
+    }
+
+    public void PushObj(GameObject obj)
+    {
+
+    }
+
+    public void PopObj()
+    {
+
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+}
