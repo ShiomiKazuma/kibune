804ab0cac77bbeec0a63abbbaa3fa50a3f0d0764
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-01-17 23:55:54 +0900

    [add]ドローンの挙動完成を追加

--- Assets/Shiomi/Script/Doroon/Doroon.cs
+++ Assets/Shiomi/Script/Doroon/Doroon.cs
@@ -16,22 +16,24 @@ public class Doroon : MonoBehaviour
     [SerializeField, Header("凝視からチェイスまでの時間"), Range(0, 10)]
     float _lookTime = 2;
     [SerializeField, Header("ドローンのスピード")]
-    float _doroonSpeed = 10;
+    float _doroonSpeed = 1;
+    [SerializeField, Header("ドローンの速度制限")]
+    float _doroonLimitSpeed = 5;
     [SerializeField, Header("ドローンのステート")]
     State _state;
     [SerializeField, Header("画像データを入れる用のゲームオブジェクト")]
     GameObject _imageObject;
     [SerializeField, Header("？マーク")]
-    Image _hatena;
+    Sprite _hatena;
     [SerializeField, Header("!マーク")]
-    Image _biltukuri;
+    Sprite _biltukuri;
     Image _image;
     [SerializeField, Header("自爆するまでの時間")]
     float _explosionTime;
     float _explosionTimer;
     bool IsChase = false;
     //プレイヤーのゲームオブジェクト
-    GameObject _player;
+    [SerializeField, Header("プレイヤー")]GameObject _player;
     //現在の凝視時間
     float _lookTimer;
     //プレイヤーのレイヤーマスク
@@ -43,16 +45,18 @@ public class Doroon : MonoBehaviour
         Look,
         Chase
     }
-
+    private void Awake()
+    {
+        //_player = GameObject.FindGameObjectWithTag("Player").GetComponent<GameObject>();
+    }
     // Start is called before the first frame update
     void Start()
     {
         _lookTimer = 0;
         _explosionTimer = 0;
-        _player = GameObject.FindGameObjectWithTag("Player").GetComponent<GameObject>();
         _state = State.Serch;
         _image = _imageObject.GetComponent<Image>();
-        _rb = this.GetComponent<Rigidbody>();
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -77,16 +81,19 @@ public class Doroon : MonoBehaviour
     {
         Debug.Log("探しています");
         //回転をリセットする
-        this.transform.rotation = Quaternion.identity;
+        //this.transform.rotation = Quaternion.identity;
         //索敵範囲内にプレイヤーがいる場合の処理
-        if(Vector3.Distance(this.transform.position, _player.gameObject.transform.position) <= _sarchRange)
+        if(Vector3.Distance(this.transform.position, _player.transform.position) < _sarchRange)
         {
             RaycastHit hit;
             Vector3 dir = (_player.transform.position - this.transform.position).normalized;
-            if (Physics.Raycast(this.transform.position, dir, out hit, _sarchRange, _layerMask))
+            if (Physics.Raycast(this.transform.position, dir, out hit, _sarchRange))
             {
-                _state = State.Look;
-                _image = _hatena;
+                if(hit.collider.gameObject.tag == "Player")
+                {
+                    _state = State.Look;
+                    _image.sprite = _hatena;
+                }
             }  
         }
     }
@@ -95,40 +102,52 @@ public class Doroon : MonoBehaviour
     void Look()
     {
         Debug.Log("凝視しています");
-        //プレイヤーの方向に向く
+        //プレイヤーの方向を取得
         Vector3 dir = (_player.transform.position - this.transform.position).normalized;
-        Quaternion quaternion = Quaternion.LookRotation(dir);
-        this.transform.rotation = Quaternion.Lerp(transform.rotation, quaternion, 0.1f);
         RaycastHit hit;
-        if (Physics.Raycast(this.transform.position, dir, out hit, _sarchRange, _layerMask))
+        if (Physics.Raycast(this.transform.position, dir, out hit, _sarchRange))
         {
-            //凝視タイマーを増やしていく
-            _lookTimer += Time.deltaTime;
-            //凝視タイマーを超えていたら、追跡モードに移行する
-            if (_lookTimer > _lookTime)
+            if (hit.collider.gameObject.tag == "Player")
             {
-                StartCoroutine("ChaseWait");
-                _state = State.Chase;
-                _image = _biltukuri;  
+                //プレイヤーの方向に向く
+                Quaternion quaternion = Quaternion.LookRotation(dir);
+                this.transform.rotation = Quaternion.Lerp(transform.rotation, quaternion, 0.1f);
+                //凝視タイマーを増やしていく
+                _lookTimer += Time.deltaTime;
+                //凝視タイマーを超えていたら、追跡モードに移行する
+                if (_lookTimer > _lookTime)
+                {
+                    //StartCoroutine("ChaseWait");
+                    _state = State.Chase;
+                    _image.sprite = _biltukuri;
+                    IsChase = true;
+                }
+            }
+            else if(!(hit.collider.gameObject.tag == "Player"))
+            {
+                //凝視時間をリセットする
+                _lookTimer = 0;
+                _image.sprite = null;
+                _state = State.Serch;
             }
         }
         else
         {
             //凝視時間をリセットする
             _lookTimer = 0;
-            _image = null;
+            _image.sprite = null;
             _state = State.Serch;
         }        
     }
 
     /// <summary> 突撃時の処理 </summary>
-    IEnumerable ChaseWait()
-    {
-        Debug.Log("突撃準備");
-        //1秒数を待つ
-        yield return new WaitForSeconds(1.0f);
-        IsChase = true;
-    }
+    //IEnumerable ChaseWait()
+    //{
+    //    Debug.Log("突撃準備");
+    //    //1秒数を待つ
+    //    yield return new WaitForSeconds(1.0f);
+    //    IsChase = true;
+    //}
 
     /// <summary> 突撃時の処理 </summary>
     void Chase()
@@ -142,12 +161,20 @@ public class Doroon : MonoBehaviour
             Quaternion quaternion = Quaternion.LookRotation(dir);
             this.transform.rotation = Quaternion.Lerp(transform.rotation, quaternion, 0.1f);
             _rb.AddForce(dir * _doroonSpeed);
+            _rb.velocity = Vector3.ClampMagnitude(_rb.velocity, _doroonLimitSpeed);
             //自爆時間を過ぎるかプレイヤーに近づくと爆発する
             if (_explosionTime < _explosionTimer || Vector3.Distance(this.transform.position, _player.transform.position) < 1)
             {
+                Debug.Log("爆発");
                 Instantiate(_explosionGameObject, this.transform.position, Quaternion.identity);
                 Destroy(this.gameObject);
             }
         }
-    }  
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _sarchRange);
+    }
 }
