f58ad7a8f909fce2ee89332a5f821d03bab747f0
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2024-01-18 09:42:10 +0900

    dasdadas

--- Assets/SuganumaDEV/Packages/SLib/LibraryCore/GameSystem/SaveData/Scripts/SaveDataChecker.cs
+++ Assets/SuganumaDEV/Packages/SLib/LibraryCore/GameSystem/SaveData/Scripts/SaveDataChecker.cs
@@ -15,45 +15,30 @@ namespace SLib
         public class SaveDataChecker : SingletonBaseClass<SaveDataChecker>
         {
             [SerializeField]
-            Button _startBtn;
+            Button _continueButton;
             [SerializeField]
             PlayerSaveDataSerializer _dataSerializer;
-            [SerializeField]
-            string _newGamedSceneName;
-            [SerializeField]
-            string _continuedSceneName;
 
-            Text _startGameButtonsText;
             GameInfo _gameInfo;
             SceneLoader _sceneLoader;
 
             protected override void ToDoAtAwakeSingleton()
             {
                 _gameInfo = GameObject.FindFirstObjectByType<GameInfo>();
-                _startGameButtonsText = _startBtn.GetComponentInChildren<Text>();
                 _sceneLoader = GameObject.FindFirstObjectByType<SceneLoader>();
 
                 if (_gameInfo.TitleSceneName != SceneManager.GetActiveScene().name) return; // If Active Scene Is NOT TitleScene, DoNothing
 
-                _startBtn.onClick.RemoveAllListeners();
-            }
-
-            private void Start()
-            {
-                UnityAction _UANewGame = new UnityAction(() => { _sceneLoader.LoadSceneByName(_newGamedSceneName); });
-                UnityAction _UAContinueGame = new UnityAction(() => { _sceneLoader.LoadSceneByName(_continuedSceneName); });
                 try
                 {
                     if (_dataSerializer.ReadSaveData() != null) // If SavedData Couldn't found , Exception Will Threw
                     {
-                        _startGameButtonsText.text = "Continue";
-                        _startBtn.onClick.AddListener(_UAContinueGame);
+                        _continueButton.interactable = true;
                     }
                 }
                 catch (FileNotFoundException)
                 {
-                    _startGameButtonsText.text = "New Game";
-                    _startBtn?.onClick.AddListener(_UANewGame);
+                    _continueButton.interactable = false;
                 }
             }
         }
