ba8e4d566bb412559a87db2b6a95c7aa682c0d8e
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2024-01-19 23:33:51 +0900

    [add] coding some functions

---
+++ Assets/SuganumaDEV/Packages/SLib/LibraryCore/GameSystem/SaveData/Scripts/DataTemplate/SettingsDataTemplate.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// auth suganuma
+public class SettingsDataTemplate : MonoBehaviour
+{
+
+    float _volMaster;       // on value changed
+    float _volBGM;
+    float _volSE;
+    int _resolutionIndex;       // on drop down value changed
+    int _refreshRateIndex;
+    int _displayIndex;
+
+
+}
