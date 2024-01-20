02a8cc4dce297c424f0003ff22d4f3d9983a1c29
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-01-20 13:30:52 +0900

    [add]新しいぐらっぷ処理を追加

---
+++ Assets/Shiomi/NewGrap/Grap/Sliding.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sliding : MonoBehaviour
+{
+    public Transform _orientation;
+    public Transform _playerObject;
+    Rigidbody _rb;
+
+    
+}
